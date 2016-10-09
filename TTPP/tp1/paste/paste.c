f1df290aa99aafdc20b855adad950a54047700be
Contre <lucascontre95@live.com>
2016-10-09 20:41:12 -0300

    paste replace

--- TTPP/tp1/paste/paste.c
+++ TTPP/tp1/paste/paste.c
@@ -21,3 +21,34 @@ $ ./paste anios.txt ciudades.txt
 2013  Lanús
 2016  Nuñez
 2016  Avellaneda*/
+#include <stdio.h>;
+#include <string.h>;
+#define SPEARADOR '\t'
+
+void error(char *message){
+    fprintf(stderr, "%s\n", message);
+}
+
+int main(int argc, char const *argv[]) {
+
+    if(argc != 2){
+        error("CANTIDAD DE ARCHIVOS NO ES LA CORRECTA !")
+        return 1;
+    }
+
+    char single_line_1[150];
+    char single_line_2[150];
+    FILE * file_1 = fopen(argv[1],'r');
+    FILE * file_2 = fopen(argv[2],'r');
+
+    while(!feof(file_1) || !feof(file_2)){
+        getline(&single_line_2,150,file_1);
+        getline(&single_line_1,150,file_2);
+        fprintf(stout, "%s\n",single_line_1);
+        fprintf(stout, "%s\n",single_line_2);
+        free(single_line_1);
+        free(single_line_2);
+    }
+
+    return 0;
+}
