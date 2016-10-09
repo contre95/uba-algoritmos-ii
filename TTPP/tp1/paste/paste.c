9733e79e7cd48c46a1add262a17cb704bbd9dc31
Contre <lucascontre95@live.com>
2016-10-09 19:54:20 -0300

    Update structure TP1

---
+++ TTPP/tp1/paste/paste.c
@@ -0,0 +1,23 @@
+/*Por ejemplo, dado el siguiente par de archivos:
+
+$ cat anios.txt
+1996
+2007
+2013
+2016
+2016
+
+$ cat ciudades.txt
+Bogotá
+La Boca
+Lanús
+Nuñez
+Avellaneda
+Al invocar el programa, su salida deberá ser la siguiente:
+
+$ ./paste anios.txt ciudades.txt
+1996  Bogotá
+2007  La Boca
+2013  Lanús
+2016  Nuñez
+2016  Avellaneda*/
