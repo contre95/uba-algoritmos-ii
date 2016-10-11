85bf9fab43e969b893887a91e0a72fdff3f6ef46
Contre <lucascontre95@live.com>
2016-10-10 23:43:31 -0300

    dc still doesn't work. and i have trouble with de dynamic array in the split function.

---
+++ TTPP/tp1/strutil_tp1/srtutil.c
@@ -0,0 +1,22 @@
+#include "strutil.h"
+#include <string.h>
+
+
+char * create_dinamic_string(size_t string_lenght){
+    char *string = malloc(size_of(char) * string_lenght);
+    return string
+}
+
+char** split(const char* str, char sep){
+    char *string_array = malloc(size_of(char) * strlen(str));
+    char character = str[0];
+    int i = 0;
+    char string[] = "";
+    while(character != '\0'){
+        if(str[i]==sep){
+
+        }
+        string+=str[i];
+    }
+
+}
