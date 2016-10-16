9bf79323ef88697685375e967b0ff9b0f448307e
Contre <lucascontre95@live.com>
2016-10-16 18:38:29 -0300

    Falta el Srtutil y el dc

--- TTPP/tp1/dc_tp1/dc.c
+++ TTPP/tp1/dc_tp1/dc.c
@@ -4,51 +4,21 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+char strdup(const char string);
+
 void error(char *message){
     fprintf(stderr, "%s\n", message);
 }
 
-char** split(const char* str, char sep){
-    int cant_pal = 1;
-    for (size_t i = 0; i < strlen(str) ; i++) {
-        if( str[i]==sep) cant_pal++;
-    }
-
-    char **string_array = malloc(sizeof(char*) * cant_pal + 1);
-
-    int num_pal = 0;
-    char palabra_stack[] = "";
-        for (size_t i = 0; i < strlen(str); i++) {
-            if( str[i]==sep){
-                char *palabra = malloc(sizeof(char) * strlen(palabra_stack));
-                string_array[num_pal] = palabra;
-                palabra_stack[0] = *"";
-                num_pal++;
-            }else{
-                strcat(palabra_stack,str[i]);
-            }
-        }
-        string_array[cant_pal+1] = NULL;
-        return string_array;
-}
-
-void free_strv(char* strv[]){
-    int i = 0;
-    while (!strv[i]) {
-        free(strv[i]);
-        i++;
-    }
-    free(strv);
-}
 
 
 int main(int argc, char const *argv[]) {
 
+    for(int i = 0; i < atoi(argv[2]) ; i++){
+    char **array = split(argv[1],' ');
+    printf("%s\n",*array[i]);
+}
 
-//    pila_t *pila_polaca = pila_crear();
-//    char *cuenta = split(argv[1]," ");
-
-    /*
     for(int entrada = 1 ; entrada < argc ; entrada++){
       printf("%s\n",argv[entrada]);
         for(int i=0; i < strlen(argv[entrada]) ; i++){
@@ -109,6 +79,6 @@ int main(int argc, char const *argv[]) {
     int resultado_final = *(int*)pila_desapilar(pila_polaca);
     printf("%i\n",resultado_final);
     }
-    pila_destruir(pila_polaca);*/
+    pila_destruir(pila_polaca);
     return 0;
 }
--- TTPP/tp1/strutil_tp1/srtutil.c
+++ TTPP/tp1/strutil_tp1/srtutil.c
@@ -1,25 +1,38 @@
-#include "strutil.h"
+#include "../tda/pila/pila.h"
+#include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+char strdup(const char string);
+
+void error(char *message){
+    fprintf(stderr, "%s\n", message);
+}
+
 
 
 char** split(const char* str, char sep){
-    cant_pal = 1;
+    int cant_pal = 1;
     for (size_t i = 0; i < strlen(str) ; i++) {
         if( str[i]==sep) cant_pal++;
     }
 
-    char *string_array = malloc(size_of(char*) * cant_pal + 1);
+    char **string_array = malloc(sizeof(char*) * cant_pal + 1);
 
-    int num_pal = 0
-    char palabra_stack[] = ""
+    int num_pal = 0;
+    char *palabra_stack = malloc(sizeof(char));
         for (size_t i = 0; i < strlen(str); i++) {
             if( str[i]==sep){
-                char *palabra = malloc(size_of(char) * strlen(palabra_stack));
-                string_array[num_pal] = palabra;
-                palabra_stack = "";
+                string_array[num_pal] = palabra_stack;
+            //    char *palabra_stack = malloc(sizeof(char));
                 num_pal++;
+
             }else{
-                strcat(palabra,str[i]);
+                char * palabra_stack2 = malloc(sizeof(char) * (strlen(palabra_stack) + 2));
+                palabra_stack2[strlen(palabra_stack2)] = str[i];
+                palabra_stack2[strlen(palabra_stack2)+1] = '\0';
+                free(palabra_stack);
+                *palabra_stack = *palabra_stack2;
             }
         }
         string_array[cant_pal+1] = NULL;
@@ -33,4 +46,17 @@ void free_strv(char* strv[]){
         i++;
     }
     free(strv);
+}
+
+
+int main(int argc, char const *argv[]) {
+
+    for(int i = 0; i < atoi(argv[2]) ; i++){
+    char **array = split(argv[1],' ');
+    printf("%s\n",*array[i]);
+}
+//free_strv(argv[1]);
+//    pila_t *pila_polaca = pila_crear();
+//    char *cuenta = split(argv[1]," ");
+    return 0;
 }
