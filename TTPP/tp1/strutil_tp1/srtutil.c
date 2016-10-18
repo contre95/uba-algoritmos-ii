bc63cc32d195384d9f175f7a45dd3c8572af16bb
Contre <lucascontre95@live.com>
2016-10-18 00:29:46 -0300

    algunos retoques

--- TTPP/tp1/strutil_tp1/srtutil.c
+++ TTPP/tp1/strutil_tp1/srtutil.c
@@ -22,21 +22,10 @@ char** split(const char* str, char sep){
     char **string_array = malloc(sizeof(char*) * (cant_pal + 1));
 
     int num_pal = 0;
-    char palabra[1000] = "";
+    char palabra[1000]; // {}
     int largo = 0;
-    for (size_t p = 0; p < cant_pal; p++) {
-        while(str[p]!=sep){
-            palabra[largo] = str[p];
-            largo += 1;
-        }
-        palabra[largo+1] = '\0';
-        string_array[num_pal] = malloc(sizeof(char)*(largo+1));
-        //fijarse si se hizo el malloc
-        strcpy(string_array[num_pal],palabra);
-        largo = 0;
-        num_pal++;
-    }
-    /*for (size_t i = 0; i < strlen(str); i++) {
+
+    for (size_t i = 0; i < strlen(str); i++) {
         palabra[largo] = str[i];
         largo += 1;
         if( str[i]==sep || i==strlen(str)-1){
@@ -45,11 +34,28 @@ char** split(const char* str, char sep){
             //fijarse si se hizo el malloc
             strcpy(string_array[num_pal],palabra);
             largo = 0;
+            memset(palabra,'\0',strlen(palabra));
             num_pal++;
         }
-    }*/
+    }
     string_array[cant_pal] = NULL;
     return string_array;
+}
+
+void join(char** strv, char sep){
+        char string[1000];
+        int p = 0;
+        int largo = 0;
+        while (strv[p]) {
+            for (size_t i = largo; i < strlen(strv[p]) ; i++) {
+                    string[i] = strv[p][i];
+            }
+        printf("%i\n",largo );
+        largo+=strlen(strv[p]);
+        p++;
+        }
+        printf("\n%s\n",string );
+    //    return &string;
 }
 
 void free_strv(char* strv[]){
@@ -67,8 +73,10 @@ int main(int argc, char const *argv[]){
 
     char **array = split(argv[1],' ');
     for(int i = 0; i < cant_palabras(argv[1],' ')  ; i++){
-        printf("\n%s\n",array[i]);
+        printf("\n%s : %i",array[i],strlen(array[i]));
     }
+    join(array,' ');
+
 free_strv(array);
     return 0;
 }
